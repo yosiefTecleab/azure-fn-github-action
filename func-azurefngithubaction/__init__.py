9477742cedcf6b7507dfe4c7edc4e23ab0f7c053
yosiefTecleab <yosieftecleab@gmail.com>
2023-06-18 12:45:07 +0200

    edited from terminal

--- func-azurefngithubaction/__init__.py
+++ func-azurefngithubaction/__init__.py
@@ -19,6 +19,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully, after edited using git action again. Pass a name in the query string or in the request body for a personalized response.",
+             "successfully from terminal edited and pushed. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
