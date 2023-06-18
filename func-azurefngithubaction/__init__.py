4ac10518040ce47d1fd7a2eb3b48bb18dd4b1b6c
yosiefTecleab <yosieftecleab@gmail.com>
2023-06-18 11:43:21 +0200

    edited added to the line tigger

--- func-azurefngithubaction/__init__.py
+++ func-azurefngithubaction/__init__.py
@@ -19,6 +19,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully, edited. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
